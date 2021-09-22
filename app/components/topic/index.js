8fe5c544b59780eb64b42a0395c4f75b952faa23
Chanda Pwapwa <mulaikopwapwa@gmail.com>
2021-09-22 08:59:37 +0200

    Dependant questions changes

--- app/components/topic/index.js
+++ app/components/topic/index.js
@@ -1,17 +1,78 @@
 import Card from "../card";
 import Question from "../question";
+import { Button } from 'react-native';
 
 const Topic = ({ color="blue", name, questions=[], onAnswerQuestion=()=>{}, onViewIssue=()=>{} }) => {
+  const [showChildren, setShowChildren] = useState([]);
+  const [test, setTest] = useState(false);
+
+
   const handleAnswer = (question) => {
-    return (answer) => onAnswerQuestion({ question, answer });
+    return (answer) => {
+      var itemsToRemove = [...question.descendants, question.id.toString()]
+      console.log("ITEMS TO REMOVE");
+      console.log(itemsToRemove);
+      if(showChildren.find(x => x.id.toString() === question.id.toString()) && question.type == "MultiChoiceQuestion" && !question.multipleAnswers) {
+        if(answer.toString().trim() == "") {
+          setShowChildren(removeMeAndMyDes(itemsToRemove));
+          //setShowChildren(showChildren.filter(obj => question.id.toString() !== obj.id.toString()));
+        } else {
+          setShowChildren([...removeMeAndMyDes(itemsToRemove), {'id' : question.id.toString(), 'answer' : answer.toString()}]);
+          //showChildren.find(x => x.id === question.id).value = answer.toString();           
+        }
+      } else if(question.type == "MultiChoiceQuestion" && !question.multipleAnswers) {
+          setShowChildren([...showChildren, {'id' : question.id.toString(), 'answer' : answer.toString()}]);
+        // ANSWER IS BEING ADDED, BUT WE NEED A RERENDER BASED ON THIS ARRAY
+      }
+    
+      console.log("SHOW CHILDREN");
+      console.log(showChildren);
+      onAnswerQuestion({ question, answer });
+    };
   };
 
-  return (
-    questions.map((props, i) => (
-      <Card color={color} heading={name} key={i} number={i + 1} outOf={questions.length}>
+  const removeMeAndMyDes = (itemsToRemove) => {
+    var newArray = showChildren.filter(obj => !itemsToRemove.includes(obj.id.toString()));
+    
+    console.log("ARRAY WITH ITEMS RMEOVED");
+    console.log(newArray);
+    return newArray;
+  }
+
+  const onPressLearnMore = () => {
+    setTest(!test);
+  }
+
+  const renderQuestionCard = (color, props, name, questions, i, showChildren) => {
+    let result;
+    // if(props.isChild) {
+    // console.log("THE NAME " + props.text);
+    // console.log("IS THIS A CHILD " + props.isChild);
+    // console.log("RESPONSE TRIGGER " + props.childResponseTrigger);
+    // console.log("THE ARRAY " + showChildren);
+    // console.log("DOES SHOW CHILDREN CONTAIN OUR RESPONSE NUMBER? " + showChildren.includes(props.childResponseTrigger.toString()));
+    // console.log("---------------------------");
+    // }
+    if(props.isChild && contains(props.childResponseTrigger, showChildren.map(a => a.answer))) {
+      result = <Card color={color} heading={name} key={i} number={i + 1} outOf={questions.length}>
+        <Text>{props.id}</Text>
+      <Question color={color} onAnswer={handleAnswer(props)} onViewIssue={onViewIssue} {...props} />
+    </Card>
+    } 
+    else if(!props.isChild) {
+      result = <Card color={color} heading={name} key={i} number={i + 1} outOf={questions.length}>
+        <Text>{props.id}</Text>
         <Question color={color} onAnswer={handleAnswer(props)} onViewIssue={onViewIssue} {...props} />
       </Card>
-    ))
+    }
+    return result;
+  };
+  // props.childResponseTrigger renderQuestionCard(color, props, name, questions, i)
+  return (
+    questions.map((props, i) => (
+      renderQuestionCard(color, props, name, questions, i, showChildren)
+     )
+    )
   );
 };
 
