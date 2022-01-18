656d5bbcb8588a76196b87b01a7f53b19ebcf59d
Chanda Pwapwa <mulaikopwapwa@gmail.com>
2022-01-18 09:03:21 +0200

    Clean up

--- app/components/topic/index.js
+++ app/components/topic/index.js
@@ -6,28 +6,32 @@ const Topic = ({ color="blue", name, questions=[], onAnswerQuestion=()=>{}, onVi
   const [showChildren, setShowChildren] = useState([]);
   const [test, setTest] = useState(false);
 
+    const handleAnswer = (question) => {
+    return async (answer) => {
+      onAnswerQuestion({ question, answer });
+      if (question.descendants.length !== 0) {
+        displayChilden(question, answer);
+      }    
+    };
+  };
 
-  const handleAnswer = (question) => {
-    requestAnimationFrame(() => {
-    return (answer) => {
+   const  displayChilden =  function(question, answer) {
+    console.log("INSIDE ASYNC EXECUTING")
+    setTimeout(function(){ 
       var itemsToRemove = [...question.descendants, question.id.toString()]
 
       if(showChildren.find(x => x.id.toString() === question.id.toString()) && question.type == "MultiChoiceQuestion" && !question.multipleAnswers) {
         if(answer.toString().trim() == "") {
-          setShowChildren(removeMeAndMyDes(itemsToRemove));
-          //setShowChildren(showChildren.filter(obj => question.id.toString() !== obj.id.toString()));
+          setShowChildren(removeMeAndMyDes(itemsToRemove));          
         } else {
-          setShowChildren([...removeMeAndMyDes(itemsToRemove), {'id' : question.id.toString(), 'answer' : answer.toString()}]);
-          //showChildren.find(x => x.id === question.id).value = answer.toString();           
+          setShowChildren([...removeMeAndMyDes(itemsToRemove), {'id' : question.id.toString(), 'answer' : answer.toString()}]);                 
         }
       } else if(question.type == "MultiChoiceQuestion" && !question.multipleAnswers) {
           setShowChildren([...showChildren, {'id' : question.id.toString(), 'answer' : answer.toString()}]);
         // ANSWER IS BEING ADDED, BUT WE NEED A RERENDER BASED ON THIS ARRAY
       }
-      onAnswerQuestion({ question, answer });
-    };
-  });
-  };
+    }, 0)
+  }
 
   const removeMeAndMyDes = (itemsToRemove) => {
     var newArray = showChildren.filter(obj => !itemsToRemove.includes(obj.id.toString()));
