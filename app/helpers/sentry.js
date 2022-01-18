fd9bae862f52938604427ba7e7c1f2f0ccc55bfa
Chanda Pwapwa <mulaikopwapwa@gmail.com>
2022-01-18 02:49:14 +0200

    Add new Sentry code

--- app/helpers/sentry.js
+++ app/helpers/sentry.js
@@ -3,8 +3,19 @@ import { dsn } from "../../config/sentry.json";
 import Constants from "expo-constants";
 
 const initializeSentry = () => {
-  Sentry.init({ dsn, enableInExpoDevelopment: false, debug: true });
-  Sentry.setRelease(Constants.manifest.revisionId);
-};
+  
+  Sentry.init({
+    dsn: dsn,
+    enableInExpoDevelopment: false,
+    release: Constants.manifest.revisionId,
+    debug: true, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
+  });
+   // Access any @sentry/react-native exports via:
+  //Sentry.Native.*
+
+  // Access any @sentry/browser exports via:
+  //Sentry.Browser.*
+  };
 
 export default initializeSentry;
+
